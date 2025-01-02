6748c4c46250d238d18d83a1dab294c6decb5e58
Cole Joseph Roberts <161370035+cjr03@users.noreply.github.com>
2025-01-02 09:47:55 -0800

    Create ProductOfArrayExceptSelf.cpp

---
+++ ProductOfArrayExceptSelf.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size(); 
+        vector<int> answer(size, 1);
+        int left = 1;
+        for(int i = 0; i < size; ++i){
+            answer[i] = left;
+            left *= nums[i];
+        }
+        int right = 1;
+        for(int i = size - 1; i >= 0; --i){
+            answer[i] *= right;
+            right *= nums[i];
+        }
+        return answer;
+    }
+};
